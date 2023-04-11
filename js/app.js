0840f6494b1a03fdc1c3fb7d4b6219160823eac3
piyijuanjis <94359698+piyijuanjis@users.noreply.github.com>
2023-04-11 15:06:12 +0200

    5/5

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
-(function(){
+/*Mapa de leaflet en la página sobre nosotros*/
+
+var map = L.map('map').setView([36.7196, -4.42002], 13);
+var marker = L.marker([36.7196, -4.45002]).addTo(map);
+var popup = L.popup()
+    .setLatLng([36.7250, -4.45002])
+    .setContent("¡Estamos aquí!")
+    .openOn(map);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 13,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+/*Slider de comentarios en la página de inicio*/
+
+(function () {
 
     const sliders = [...document.querySelectorAll('.testimony__body')];
     const buttonNext = document.querySelector('#next');
@@ -6,26 +22,27 @@
 
     let value;
 
-    buttonNext.addEventListener('click', ()=>{
+    buttonNext.addEventListener('click', () => {
         changePosition(1);
     });
 
-    buttonBefore.addEventListener('click', ()=>{
+    buttonBefore.addEventListener('click', () => {
         changePosition(-1);
     });
 
-    const changePosition = (add)=>{
+    const changePosition = (add) => {
         const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
         value = Number(currentTestimony);
-        value +=  add;
+        value += add;
 
-        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
-        if(value === sliders.length+1 || value === 0){
+        sliders[Number(currentTestimony) - 1].classList.remove('testimony__body--show');
+        if (value === sliders.length + 1 || value === 0) {
             value = value === 0 ? sliders.length : 1;
         }
 
-        sliders[value-1].classList.add('testimony__body--show');
+        sliders[value - 1].classList.add('testimony__body--show');
 
     }
 
 })();
+
